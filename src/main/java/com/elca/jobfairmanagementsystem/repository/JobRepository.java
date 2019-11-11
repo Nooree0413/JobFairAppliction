5cd135f0b3280105eab7395d34a35b5f7b1cf360
Akash <anghud.bhowaniah@hotmail.com>
2019-11-11 10:32:56 +0400

    created job and venue repo

--- src/main/java/com/elca/jobfairmanagementsystem/repository/JobRepository.java
+++ src/main/java/com/elca/jobfairmanagementsystem/repository/JobRepository.java
@@ -1,14 +1,9 @@
 package com.elca.jobfairmanagementsystem.repository;
 
+import com.elca.jobfairmanagementsystem.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.elca.jobfairmanagementsystem.entity.Job;
-
-/**
- *
- * @author ghr
- */
 @Repository
-public interface JobRepository extends JpaRepository<Job , Long> {
+public interface JobRepository extends JpaRepository<Long, Job> {
 }
--- src/main/java/com/elca/jobfairmanagementsystem/repository/VenueRepository.java
+++ src/main/java/com/elca/jobfairmanagementsystem/repository/VenueRepository.java
@@ -1,14 +1,9 @@
 package com.elca.jobfairmanagementsystem.repository;
 
+import com.elca.jobfairmanagementsystem.entity.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.elca.jobfairmanagementsystem.entity.Venue;
-
-/**
- *
- * @author ghr
- */
 @Repository
-public interface VenueRepository extends JpaRepository<Venue,Long> {
+public interface VenueRepository extends JpaRepository<Long, Venue> {
 }

f5d312fa63ec730c567d664cea99564e041d9c7d
Ganesh Heerah <ganesh.heerah@elca.mu>
2019-11-12 10:45:35 +0400

    update candidate impl

--- src/main/java/com/elca/jobfairmanagementsystem/service/impl/CandidateScreeningImpl.java
+++ src/main/java/com/elca/jobfairmanagementsystem/service/impl/CandidateScreeningImpl.java
@@ -1,5 +1,8 @@
 package com.elca.jobfairmanagementsystem.service.impl;
 
+import java.util.List;
+
+import com.elca.jobfairmanagementsystem.dto.CandidateScreeningDto;
 import com.elca.jobfairmanagementsystem.service.CandidateScreeningService;
 
 /**
@@ -7,4 +10,8 @@ import com.elca.jobfairmanagementsystem.service.CandidateScreeningService;
  * @author ghr
  */
 public class CandidateScreeningImpl implements CandidateScreeningService {
+    @Override
+    public List<CandidateScreeningDto> findAllCandidateScreening() {
+        return null;
+    }
 }

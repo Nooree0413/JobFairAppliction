509f122d11895b6fe47de6c50bb4aa3de2bb7c8e
Nooree0413 <bibi.falak.nooree.khayrattee@elca.mu>
2019-11-13 11:24:04 +0400

    corrected path

--- src/main/java/com/elca/jobfairmanagementsystem/controller/SkillController.java
+++ src/main/java/com/elca/jobfairmanagementsystem/controller/SkillController.java
@@ -1,11 +1,12 @@
 package com.elca.jobfairmanagementsystem.controller;
 
 import com.elca.jobfairmanagementsystem.dto.SkillDto;
-import com.elca.jobfairmanagementsystem.mapper.SkillMapper;
 import com.elca.jobfairmanagementsystem.service.SkillService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  *
@@ -17,18 +18,29 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/skill/")
 public class SkillController {
     private final SkillService skillService;
-    private final SkillMapper skillMapper;
 
-    public SkillController(SkillService skillService, SkillMapper skillMapper){
-        this.skillMapper=skillMapper;
+    public SkillController(SkillService skillService){
         this.skillService=skillService;
     }
-    @PostMapping()
+    @PostMapping("/addSkill")
     public ResponseEntity saveSkill(@RequestBody SkillDto skillDto){
         skillService.saveSkill(skillDto);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-//    @PutMapping(".update/{id}")
-//    public ResponseEntity updateSkill
+    @PutMapping("update/{skillId}")
+    public ResponseEntity updateSkill(@RequestBody SkillDto skillDto){
+        skillService.updateSkill(skillDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("getSkill/{skillId}")
+    public ResponseEntity<SkillDto> getSkillById(@PathVariable Long skillId){
+        return new ResponseEntity<>(skillService.findSkillById(skillId),HttpStatus.FOUND);
+    }
+
+    @GetMapping("allSkills")
+    public ResponseEntity<List<SkillDto>> getAllSkills(){
+        return new ResponseEntity<>(skillService.searchAllSkills(),HttpStatus.FOUND);
+    }
 }

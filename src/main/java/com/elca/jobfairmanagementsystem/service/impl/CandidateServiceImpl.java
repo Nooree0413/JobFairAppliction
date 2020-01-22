fd8329d315dc432cd332fae5852f26265fd1043d
Akash <anghud.bhowaniah@hotmail.com>
2020-01-22 09:17:04 +0400

    fix bug for file upload

--- src/main/java/com/elca/jobfairmanagementsystem/service/impl/CandidateServiceImpl.java
+++ src/main/java/com/elca/jobfairmanagementsystem/service/impl/CandidateServiceImpl.java
@@ -81,7 +81,7 @@ public class CandidateServiceImpl implements CandidateService {
         List<CandidateVenueJob> candidateVenueJobs = new ArrayList<>();
         candidateVenueJobSaveList.forEach(candidatesVenueDto -> {
             try {
-                VenueJob venueJob = venueJobService.findByVenueIdAndJobId(candidatesVenueDto.getVenueId().getVenueId(), candidatesVenueDto.getJobId());
+                VenueJob venueJob = venueJobService.findByVenueIdAndJobId(candidatesVenueDto.getVenueId(), candidatesVenueDto.getJobId());
                 candidatesVenueDto.setVenueJobId(venueJob.getVenueJobId());
                 candidatesVenueDto.setJobId(venueJob.getJob().getJobId());
                 CandidateVenueJob saveCandidateVenueJob = candidateVenueJobMapper.candidateVenueJobSaveDtoToEntity(candidatesVenueDto);
@@ -144,7 +144,7 @@ public class CandidateServiceImpl implements CandidateService {
         List<CandidateVenueJob> candidateVenueJobs = new ArrayList<>();
         candidateVenueJobSaveList.forEach(candidatesVenueDto -> {
             try {
-                VenueJob venueJob = venueJobService.findByVenueIdAndJobId(candidatesVenueDto.getVenueId().getVenueId(), candidatesVenueDto.getJobId());
+                VenueJob venueJob = venueJobService.findByVenueIdAndJobId(candidatesVenueDto.getVenueId(), candidatesVenueDto.getJobId());
                 candidatesVenueDto.setVenueJobId(venueJob.getVenueJobId());
                 candidatesVenueDto.setJobId(venueJob.getJob().getJobId());
                 CandidateVenueJob saveCandidateVenueJob = candidateVenueJobMapper.candidateVenueJobSaveDtoToEntity(candidatesVenueDto);

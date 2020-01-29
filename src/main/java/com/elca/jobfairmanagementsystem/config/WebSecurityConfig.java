ae18e748eec7773222b591be5e6cd8e9b3e85a8b
Akash <anghud.bhowaniah@hotmail.com>
2020-01-29 09:00:21 +0400

    added web security ignore

--- src/main/java/com/elca/jobfairmanagementsystem/config/WebSecurityConfig.java
+++ src/main/java/com/elca/jobfairmanagementsystem/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -53,8 +54,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http
-                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/venue/active","/venue-job/jobs/title","/venue-job/jobs/category/title","/venue-jobs/jobs/category","/venue-jobs/jobs/level","/skill/all","/candidate","/candidate/candidate-cv");
     }
 
     @Bean
